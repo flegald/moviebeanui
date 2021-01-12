6d276f42af75ab11c816df00da5868daf6078927
flegald <flegal.david@protonmail.com>
2021-01-11 17:19:29 -0800

    api base

--- src/service/service.js
+++ src/service/service.js
@@ -1,5 +1,5 @@
-const API_BASE = "http://127.0.0.1:8000"
-// const API_BASE = "https://moviebeanapi.herokuapp.com"
+// const API_BASE = "http://127.0.0.1:8000"
+const API_BASE = "https://moviebeanapi.herokuapp.com"
 
 
 export const serviceCall = (url, method, data= null, token= null) => {
